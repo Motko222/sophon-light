3d2b943e8a3e64dc02046a94e49ee71f0aa98371
Motko <77466238+Motko222@users.noreply.github.com>
2024-08-31 22:15:52 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,5 +6,5 @@ curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add - && 
 sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin -y
 docker pull nillion/retailtoken-accuser:v1.0.0
 mkdir -p ~/nillion/accuser
-docker run -v ./nillion/accuser:/var/tmp nillion/retailtoken-accuser:v1.0.0 initialise
-sudo cat /root/nillion/accuser/credentials.json
+docker run -v ~/nillion/accuser:/var/tmp nillion/retailtoken-accuser:v1.0.0 initialise
+sudo cat ~/nillion/accuser/credentials.json
