346000c64620fe3a8eb4ca813dc2ffef1ed0f128
Motko <77466238+Motko222@users.noreply.github.com>
2024-09-01 22:02:40 +0200

    Update report.sh

--- report.sh
+++ report.sh
@@ -14,7 +14,7 @@ cd ~/nillion/accuser
 container=$(docker ps | grep nillion | awk '{print $NF}')
 [ $container ] && docker_status=$(docker inspect $container | jq -r .[].State.Status)
 last_challenge=$(docker logs $container | grep -a "Challenges sent to chain" | awk '{print $1}' | tail -1 | sed -r "s/\x1B\[([0-9]{1,3}(;[0-9]{1,2};?)?)?[mGK]//g")
-last_challenge_sec="$(( $(date +%s) - $(date -d $last_challenge +%s) ))s ago"
+last_challenge_sec="$(( $(date +%s) - $(date -d $last_challenge +%s) ))s"
 local_height=$(docker logs $container | grep -a "Sent block @ height" | tail -1 | awk '{print $NF}')
 is_accusing=$(docker logs $container | grep accusing | tail -1 | grep -c "Accuser IS accusing")
 sent=$(docker logs $container | grep -a "Challenges sent to Nilchain" | tail -1 | awk '{print $NF}')
