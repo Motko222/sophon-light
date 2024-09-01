f42f6d81bb86534b7f198a35ae218568476d3382
Motko <77466238+Motko222@users.noreply.github.com>
2024-09-01 22:01:06 +0200

    Update report.sh

--- report.sh
+++ report.sh
@@ -14,14 +14,15 @@ cd ~/nillion/accuser
 container=$(docker ps | grep nillion | awk '{print $NF}')
 [ $container ] && docker_status=$(docker inspect $container | jq -r .[].State.Status)
 last_challenge=$(docker logs $container | grep -a "Challenges sent to chain" | awk '{print $1}' | tail -1 | sed -r "s/\x1B\[([0-9]{1,3}(;[0-9]{1,2};?)?)?[mGK]//g")
-last_challenge_sec=$(( $(date +%s) - $(date -d $last_challenge +%s) ))
+last_challenge_sec=$(( $(date +%s) - $(date -d $last_challenge +%s) ))s ago
 local_height=$(docker logs $container | grep -a "Sent block @ height" | tail -1 | awk '{print $NF}')
 is_accusing=$(docker logs $container | grep accusing | tail -1 | grep -c "Accuser IS accusing")
+sent=$(docker logs $container | grep -a "Challenges sent to Nilchain" | tail -1 | awk '{print $NF}')
 
 version=?
 
 case $docker_status$is_accusing in
-  running1) status=ok; message="last=$last_challenge_sec" ;;
+  running1) status=ok; message="last=$last_challenge_sec sent=$sent" ;;
   running0) status=warning; message="not accusing" ;;
   *) status="error"; message="docker not running" ;;
 esac
@@ -42,6 +43,7 @@ cat << EOF
    { "key":"docker_status","value":"$docker_status" },
    { "key":"local_height","value":"$local_height" },
    { "key":"last_challenge_sec","value":"$last_challenge_sec" },
+   { "key":"sent","value":"$sent" },
    { "key":"is_accusing","value":"$is_accusing" }
   ]
 }
