2661ee6d324818d5ab8b254cc34e2b7f5c360b40
Motko <77466238+Motko222@users.noreply.github.com>
2024-09-02 11:34:14 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -5,6 +5,7 @@ rpc=https://nillion-testnet-rpc.polkachu.com
 
 container=$(docker ps | grep nillion | awk '{print $NF}')
 docker stop $container
+docker rm $container
 read -p "Block? " block
 docker run -d -v ~/nillion/accuser:/var/tmp nillion/retailtoken-accuser:v1.0.0 accuse --rpc-endpoint $rpc --block-start $block
 container=$(docker ps | grep nillion | awk '{print $NF}')
