5e2ff809837e688c3bdd47299434f6e57a89b71e
Motko <77466238+Motko222@users.noreply.github.com>
2024-09-04 09:00:30 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
+folder=$(echo $(cd -- $(dirname -- "${BASH_SOURCE[0]}") && pwd)
+cd $folder
+
 rpc=https://nillion-testnet-rpc.polkachu.com
 #rpc=https://testnet-nillion-rpc.lavenderfive.com
 #rpc=https://nillion-testnet.rpc.kjnodes.com
 
-container=$(docker ps -a | grep nillion | awk '{print $NF}')
-docker stop $container
-docker rm $container
+./stop.sh
+
 read -p "Block? " block
 docker run -d -v ~/nillion/accuser:/var/tmp nillion/retailtoken-accuser:v1.0.0 accuse --rpc-endpoint $rpc --block-start $block
 container=$(docker ps | grep nillion | awk '{print $NF}')
