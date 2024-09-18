affca39743f56f1bdebbf578b747b83d79802cf9
Motko <77466238+Motko222@users.noreply.github.com>
2024-09-18 22:14:59 +0200

    Update initialise.sh

--- initialise.sh
+++ initialise.sh
@@ -2,8 +2,10 @@
 
 path=$(cd -- $(dirname -- "${BASH_SOURCE[0]}") && pwd)
 folder=$(echo $path | awk -F/ '{print $NF}')
+cd $path
+source config
 
 cd ~
 mkdir -p ~/$folder/accuser
-docker run -v ~/$folder/accuser:/var/tmp nillion/retailtoken-accuser:v1.0.1 initialise
+docker run -v ~/$folder/accuser:/var/tmp nillion/retailtoken-accuser:$VERSION initialise
 sudo cat ~/$folder/accuser/credentials.json
