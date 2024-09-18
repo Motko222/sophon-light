6240113a0f3673e0083d09c88f63bf84b0a5715b
Motko <77466238+Motko222@users.noreply.github.com>
2024-09-18 15:50:21 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -7,4 +7,4 @@ sudo apt install apt-transport-https ca-certificates curl software-properties-co
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
 sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin -y
-docker pull nillion/retailtoken-accuser:v1.0.1
+docker pull nillion/retailtoken-accuser:v2.0.3
