9082652ffb77003d235d916a32d5cc340c0fe9c3
Motko <77466238+Motko222@users.noreply.github.com>
2024-09-25 00:44:52 +0200

    Update report.sh

--- report.sh
+++ report.sh
@@ -11,20 +11,20 @@ tail=100000
 
 cd ~/$folder/accuser
 
-container=$(docker ps | grep $folder | awk '{print $NF}')
+container=$(docker ps | grep nillion | awk '{print $NF}')
 [ $container ] && docker_status=$(docker inspect $container | jq -r .[].State.Status)
 last_challenge=$(docker logs --tail $tail $container | grep -a "Challenges sent to chain" | awk '{print $1}' | tail -1 | sed -r "s/\x1B\[([0-9]{1,3}(;[0-9]{1,2};?)?)?[mGK]//g")
 [ -z $last_challenge ] && last_challenge_sec=never || last_challenge_sec="$(( $(date +%s) - $(date -d $last_challenge +%s) ))s"
 local_height=$(docker logs --tail $tail $container | grep -a "Sent block @ height" | tail -1 | awk '{print $NF}')
 is_accusing=$(docker logs --tail $tail $container | grep accusing | tail -1 | grep -c "Accuser IS accusing")
-registered=$(docker logs --tail $tail $container | grep "Registered:" | tail -1 | grep -c "Registered: true")
+verifying=$(docker logs --tail $tail $container | grep "Verifying" | tail -1 | grep -c "Verifying: true")
 sent=$(docker logs --tail $tail $container | grep -a "Challenges sent to Nilchain" | tail -1 | awk '{print $NF}')
-url=$(docker ps -a --no-trunc | grep $folder | awk -F '--rpc-endpoint ' '{print $2}' | awk '{print $1}')
-version=$(docker ps -a --no-trunc | grep $folder | awk -F 'accuser:' '{print $2}' | awk '{print $1}')
+url=$(ps aux | grep nillion | grep -v grep | awk -F '--rpc-endpoint ' '{print $2}' | awk '{print $1}')
+version=$(docker ps -a --no-trunc | grep nillion | awk -F 'verifier:' '{print $2}' | awk '{print $1}')
 
-case $docker_status$is_accusing in
+case $docker_status$verifying in
   running1) status=ok; message="last=$last_challenge_sec sent=$sent" ;;
-  running0) status=warning; message="not accusing height=$local_height" ;;
+  running0) status=warning; message="not verifying, sent=$sent" ;;
   *) status="error"; message="docker not running" ;;
 esac
 
