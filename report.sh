026d9461e2d6f818722e31ed9e1843ff1d491015
Motko <77466238+Motko222@users.noreply.github.com>
2024-09-25 22:33:13 +0200

    Update report.sh

--- report.sh
+++ report.sh
@@ -16,11 +16,12 @@ container=$(docker ps | grep $folder | awk '{print $NF}')
 last_challenge=$(docker logs --tail $tail $container | grep -a "Challenge sent to Nilchain" | awk '{print $1}' | tail -1 | sed -r "s/\x1B\[([0-9]{1,3}(;[0-9]{1,2};?)?)?[mGK]//g")
 [ -z $last_challenge ] && last_challenge_sec=never || last_challenge_sec="$(( $(date +%s) - $(date -d $last_challenge +%s) ))s"
 local_height=$(docker logs --tail $tail $container | grep -a "Sent block @ height" | tail -1 | awk '{print $NF}')
-is_accusing=$(docker logs --tail $tail $container | grep accusing | tail -1 | grep -c "Accuser IS accusing")
+registered=$(docker logs --tail $tail $container | grep accusing | tail -1 | grep -c "Verifier is registered")
 verifying=$(docker logs --tail $tail $container | grep "Verifying" | tail -1 | grep -c "Verifying: true")
 sent=$(docker logs --tail $tail $container | grep -a "Challenges sent to Nilchain" | tail -1 | awk '{print $NF}')
 #url=$(ps aux | grep nillion | grep -v grep | awk -F '--rpc-endpoint ' '{print $2}' | awk '{print $1}')
 version=$(docker ps -a --no-trunc | grep $folder | awk -F 'verifier:' '{print $2}' | awk '{print $1}')
+address=$(docker logs --tail $tail $container | grep -a "address: " | tail -1 | awk '{print $NF}')
 
 case $docker_status$verifying in
   running1) status=ok; message="last=$last_challenge, sent=$sent" ;;
@@ -47,11 +48,11 @@ cat >$json << EOF
    "local_height":"$local_height",
    "last_challenge_sec":"$last_challenge_sec",
    "sent":"$sent",
-   "is_accusing":"$is_accusing",
    "verifying":"$verifying",
    "registered":"$registered",
    "url":"$url",
-   "version":"$version"
+   "version":"$version",
+   "address":"$address"
   }
 }
 EOF
