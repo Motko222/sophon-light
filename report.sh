ab5d973310a3ae5288b4e7f6fb23d591ea00e25c
Motko <77466238+Motko222@users.noreply.github.com>
2024-09-25 22:40:43 +0200

    Update report.sh

--- report.sh
+++ report.sh
@@ -15,11 +15,11 @@ container=$(docker ps | grep $folder | awk '{print $NF}')
 [ $container ] && docker_status=$(docker inspect $container | jq -r .[].State.Status)
 last_challenge=$(docker logs --tail $tail $container | grep -a "Challenge sent to Nilchain" | awk '{print $1}' | tail -1 | sed -r "s/\x1B\[([0-9]{1,3}(;[0-9]{1,2};?)?)?[mGK]//g")
 [ -z $last_challenge ] && last_challenge_sec=never || last_challenge_sec="$(( $(date +%s) - $(date -d $last_challenge +%s) ))s"
-local_height=$(docker logs --tail $tail $container | grep -a "Sent block @ height" | tail -1 | awk '{print $NF}')
+local_height=$(docker logs --tail $tail $container | grep -a "Got block @ height" | tail -1 | awk '{print $NF}')
 registered=$(docker logs --tail $tail $container | grep -c "Verifier is registered" | tail -1 | wc -l)
 verifying=$(docker logs --tail $tail $container | grep "Verifying" | tail -1 | grep -c "Verifying: true")
 sent=$(docker logs --tail $tail $container | grep -a "Challenges sent to Nilchain" | tail -1 | awk '{print $NF}')
-url=$(docker ps -a --no-trunc | grep $folder | awk -F '--rpc-endpoint' '{print $2}' | awk '{print $1}' | sed '/\"//g')
+url=$(docker ps -a --no-trunc | grep $folder | awk -F '--rpc-endpoint' '{print $2}' | awk '{print $1}' | sed 's/\"//g')
 version=$(docker ps -a --no-trunc | grep $folder | awk -F 'verifier:' '{print $2}' | awk '{print $1}')
 address=$(docker logs --tail $tail $container | grep -a "address: " | tail -1 | awk '{print $NF}')
 
