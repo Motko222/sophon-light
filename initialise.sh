c14899579586e2f582a71bf8b91b494cfb0f2cae
Motko <77466238+Motko222@users.noreply.github.com>
2024-10-03 12:22:59 +0200

    Update initialise.sh

--- initialise.sh
+++ initialise.sh
@@ -3,11 +3,10 @@
 path=$(cd -- $(dirname -- "${BASH_SOURCE[0]}") && pwd)
 folder=$(echo $path | awk -F/ '{print $NF}')
 cd $path
+cp config.sample config
 source config
 
 cd ~
 mkdir -p ~/$folder/verifier
 docker run -v ~/$folder/verifier:/var/tmp nillion/verifier:$VERSION initialise
 sudo cat ~/$folder/verifier/credentials.json
-
-cp config.sample config
