f227d65aac1be355c9ab837fa281edf76bc64c54
Motko <77466238+Motko222@users.noreply.github.com>
2024-10-03 11:37:04 +0200

    Update start-select.sh

--- start-select.sh
+++ start-select.sh
@@ -5,21 +5,25 @@ folder=$(echo $path | awk -F/ '{print $NF}')
 cd $path
 source config
 
-
-echo "------------------------"
-cat rpc | nl
-echo "------------------------"
-read -p "? " n
-
-if [[ $n == ?(-)+([0-9]) ]]
-  then
-    rpc=$(cat rpc | head -$n | tail -1 )
-  else 
-    exit 1
+if [ -z $RPC ]
+then
+    echo "------------------------"
+    cat rpc | nl
+    echo "------------------------"
+    read -p "? " n
+    
+    if [[ $n == ?(-)+([0-9]) ]]
+      then
+        rpc=$(cat rpc | head -$n | tail -1 )
+      else 
+        exit 1
+    fi
+else
+    rpc=$RPC
 fi
 
 ./stop.sh
 
-docker run -d --name $folder -v ~/$folder/verifier:/var/tmp nillion/verifier:$VERSION verify --rpc-endpoint $rpc
+docker run -d --name $folder -v ~/$folder/verifier:/var/tmp nillion/verifier:$VERSION verify --rpc-endpoint $rpc --restart always
 container=$(docker ps | grep $folder | awk '{print $NF}')
 docker logs -n 200 -f $container
