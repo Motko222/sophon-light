fe50e8bb5439dc8d4fa39fc64954f0a602637d03
Motko <77466238+Motko222@users.noreply.github.com>
2024-10-03 11:16:21 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -5,16 +5,21 @@ folder=$(echo $path | awk -F/ '{print $NF}')
 cd $path
 source config
 
-echo "------------------------"
-cat rpc | nl
-echo "------------------------"
-read -p "? " n
-
-if [[ $n == ?(-)+([0-9]) ]]
-  then
-    rpc=$(cat rpc | head -$n | tail -1 )
-  else 
-    exit 1
+if [ -z $RPC ]
+then 
+      echo "------------------------"
+      cat rpc | nl
+      echo "------------------------"
+      read -p "? " n
+      
+      if [[ $n == ?(-)+([0-9]) ]]
+        then
+          rpc=$(cat rpc | head -$n | tail -1 )
+        else 
+          exit 1
+      fi
+else
+      rpc=$RPC
 fi
 
 ./stop.sh
