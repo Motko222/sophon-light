e15bfc9e6ab6a0f15c7b1b0f015e4ef57f2408f8
Motko <77466238+Motko222@users.noreply.github.com>
2024-10-03 11:27:25 +0200

    Update start-select.sh

--- start-select.sh
+++ start-select.sh
@@ -5,21 +5,17 @@ folder=$(echo $path | awk -F/ '{print $NF}')
 cd $path
 source config
 
-if [ -z $RPC ]
-then 
-      echo "------------------------"
-      cat rpc | nl
-      echo "------------------------"
-      read -p "? " n
-      
-      if [[ $n == ?(-)+([0-9]) ]]
-        then
-          rpc=$(cat rpc | head -$n | tail -1 )
-        else 
-          exit 1
-      fi
-else
-      rpc=$RPC
+
+echo "------------------------"
+cat rpc | nl
+echo "------------------------"
+read -p "? " n
+
+if [[ $n == ?(-)+([0-9]) ]]
+  then
+    rpc=$(cat rpc | head -$n | tail -1 )
+  else 
+    exit 1
 fi
 
 ./stop.sh
