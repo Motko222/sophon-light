63b9b953bec77911aaef419a940433f3f7f28255
Motko <77466238+Motko222@users.noreply.github.com>
2024-12-24 00:09:31 +0100

    Update remove.sh

--- remove.sh
+++ remove.sh
@@ -2,16 +2,10 @@
 
 path=$(cd -- $(dirname -- "${BASH_SOURCE[0]}") && pwd)
 folder=$(echo $path | awk -F/ '{print $NF}')
+cd $path
+source config
 
 read -p "Are you sure? " sure
-case $sure in y|Y|yes|YES|Yes) ;; *) exit ;; esac
-
-#backup
-[ -d /root/backup/$folder ] || mkdir -p /root/backup/$folder
-cp /root/$folder/verifier/credentials.json /root/backup/$folder
-cat /root/$folder/verifier/credentials.json
-echo
-read -p "Backup your credentials... Continue? " sure
 case $sure in y|Y|yes|YES|Yes) ;; *) exit ;; esac
 
 echo "Stopping containers..."
@@ -23,14 +17,7 @@ container=$(docker ps -a | grep $folder | awk '{print $NF}')
 docker rm $container 
 
 echo "Removing images..."
-docker image rm nillion/verifier:v1.0.0
-docker image rm nillion/verifier:v1.0.1
-docker image rm nillion/retailtoken-accuser:v1.0.0
-docker image rm nillion/retailtoken-accuser:v1.0.1
-
-echo "Deleting folder..."
-rm -r /root/$folder
-echo /root/$folder
+docker image rm sophonhub/sophon-light-node:$VERSION
 
 echo "Deleting scripts..."
 rm -r /root/scripts/$folder
