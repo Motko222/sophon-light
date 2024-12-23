044816055476ce773f1f8d3c8024aab78b2accfb
Motko <77466238+Motko222@users.noreply.github.com>
2024-12-23 23:59:51 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,11 +5,8 @@ folder=$(echo $path | awk -F/ '{print $NF}')
 cd $path
 source config
 
-cd ~
 sudo apt update
 sudo apt upgrade -y
-sudo apt install apt-transport-https ca-certificates curl software-properties-common -y
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
-sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin -y
-docker pull nillion/verifier:$VERSION
+sudo apt install docker.io -y
+sudo systemctl enable docker
+docker pull sophonhub/sophon-light-node:$VERSION
