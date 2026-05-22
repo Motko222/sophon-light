eb809464173b5e9b8adce9c9b4734ed03b4d3949
Motko <77466238+Motko222@users.noreply.github.com>
2026-05-22 22:57:50 +0200

    Update status.sh

--- status.sh
+++ status.sh
@@ -1,12 +1,6 @@
 #!/bin/bash
 
 path=$(cd -- $(dirname -- "${BASH_SOURCE[0]}") && pwd)
-folder=$(echo $path | awk -F/ '{print $NF}')
-source /root/.bash_profile
-cd $path
-source config
-
-chain=sophon
-network=mainnet
+source $path/config
 
 curl -sX GET "https://monitor.sophon.xyz/nodes?operators=$OPERATOR" | jq
