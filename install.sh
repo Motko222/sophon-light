5b69c25dc572059d0cf68a981ee4bd9b84e2d233
Motko <77466238+Motko222@users.noreply.github.com>
2026-05-22 10:07:10 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,8 +5,8 @@ folder=$(echo $path | awk -F/ '{print $NF}')
 cd $path
 source config
 
-sudo apt update
-sudo apt upgrade -y
-sudo apt install docker.io -y
-sudo systemctl enable docker
+#sudo apt update
+#sudo apt upgrade -y
+#sudo apt install docker.io -y
+#sudo systemctl enable docker
 docker pull sophonhub/sophon-light-node:$VERSION
